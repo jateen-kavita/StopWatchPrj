6ceea3f5021fa9df497f11f48abb3fad583b19ee
jateen-kavita <jateenpatil2002@gmail.com>
2024-08-02 13:01:30 +0530

    First Commit

---
+++ Index.js
@@ -0,0 +1,90 @@
+const hours = document.querySelector(".hour-container");
+const minutes = document.querySelector(".minute-container");
+const second = document.querySelector(".second-container");
+const startbtn = document.querySelector(".start-btn");
+const pausebtn = document.querySelector(".pause-btn");
+const resetbtn = document.querySelector(".reset-btn");
+
+
+// Toggle theme
+let slider = document.querySelector(".slider");
+let togglebtn = document.querySelector(".theme-bg");
+let darkMode = "darkMode";
+let lightMode = "lightMode";
+
+const slide = (switchbtn) => {
+  switchbtn.addEventListener("click", () => {
+    if (slider.classList.contains(darkMode)) {
+      slider.style.transform = "translateX(25px)";
+      togglebtn.style.backgroundColor = 'rgba(255, 255, 255, 0.75)'
+    } else {
+      slider.style.transform = "translateX(0px)";
+      togglebtn.style.backgroundColor = 'rgba(17, 25, 40, 0.75)'
+    }
+    document.querySelectorAll('.lightMode, .darkMode').forEach(theme => {
+      if (theme.classList.contains(lightMode)) {
+        theme.classList.remove(lightMode);
+        theme.classList.add(darkMode);
+      } else {
+        theme.classList.remove(darkMode);
+        theme.classList.add(lightMode);
+      }
+    });
+  });
+};
+slide(togglebtn);
+
+
+//stopwatch Logic
+let [hrs, min, sec] = [0, 0, 0];
+
+let timer = 0;
+
+const stopwatch = () => {
+  sec++;
+  if (sec === 60) {
+    sec = 0;
+    min++;
+    if (min === 60) {
+      min = 0;
+      hrs++;
+    }
+  }
+  second.textContent = sec;
+  minutes.textContent = min;
+  hours.textContent = hrs;
+};
+
+const start = (startTime) => {
+  startTime.addEventListener("click", () => {
+    if (!timer) {
+      timer = setInterval(stopwatch, 1000);
+    }
+  });
+};
+
+const pause = (pauseTime) => {
+  pauseTime.addEventListener("click", () => {
+    second.textContent = sec;
+    minutes.textContent = min;
+    hours.textContent = hrs;
+    clearInterval(timer);
+    timer = null;
+  });
+};
+
+const reset = (resetTime) => {
+  resetTime.addEventListener("click", () => {
+    clearInterval(timer);
+    timer = null;
+    [hrs, min, sec] = [0, 0, 0];
+    second.textContent = sec;
+    minutes.textContent = min;
+    hours.textContent = hrs;
+  });
+};
+
+start(startbtn);
+pause(pausebtn);
+reset(resetbtn);
+
